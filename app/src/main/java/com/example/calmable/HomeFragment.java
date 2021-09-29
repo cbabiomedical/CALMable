352c2ee560e8bf7d935b777060ce46e58ad5c619
Mahidi99 <cbacdabiomedical@gmail.com>
2021-09-29 15:51:34 +0530

    Updated Landing page by Mahidi

--- app/src/main/java/com/example/calmable/HomeFragment.java
+++ app/src/main/java/com/example/calmable/HomeFragment.java
@@ -2,44 +2,47 @@ package com.example.calmable;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeFragment extends Fragment {
 
 
-    private Button btn1, btn2,btn3;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
+    private Button stressHomeBtn, sleepyHomebtn,motivateHomeBtn, happyHomeBtn, breathHomeBtn;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        stressHomeBtn = (Button) view.findViewById(R.id.stressHomeBtn);
+        stressHomeBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(getActivity(),stressHome.class));
+
+            }
+        });
 
 
-//        Button button = (Button) view.findViewById(R.id.btn_conferma);
-//        button.setOnClickListener(new View.OnClickListener()
-//        {
-//            @Override
-//            public void onClick(View v)
-//            {
-//                // do something
-//            }
-//        });
+        return view;
 
     }
 
