884d2a7c59cdb1dd26af9074094cf3274b8dc308
Mahidi99 <cbacdabiomedical@gmail.com>
2021-10-07 05:25:22 +0530

    Updated Landing page by Mahidi

--- app/src/main/java/com/example/calmable/HomeFragment.java
+++ app/src/main/java/com/example/calmable/HomeFragment.java
@@ -38,14 +38,19 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         TextView txtProgress = (TextView) view.findViewById(R.id.txtProgress);
+        TextView txtProgress2 = (TextView) view.findViewById(R.id.txtPastProgress);
         ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressBar2);
 
 
-        int heartrate = 158;
-        String hr = getColoredSpanned(Integer.toString(heartrate), "#800000");
+        int CurrentHeartRate = 158; //user's current heart rate
+        int PastHeartRate = 112; //user's past heart rate
+        int PastTime = 5; //time gap between user's current heart rate
+        String chr = getColoredSpanned(Integer.toString(CurrentHeartRate), "#800000");
+        String phr = getColoredSpanned(Integer.toString(PastHeartRate), "#000000");
+        String t = getColoredSpanned(Integer.toString(PastTime), "#000000");
         String BPM = getColoredSpanned("\u1D2E\u1D3E\u1D39","#000000");
-        txtProgress.setText(Html.fromHtml(hr+" "+BPM));
-
+        txtProgress.setText(Html.fromHtml(chr+" "+BPM));
+        txtProgress2.setText(Html.fromHtml(phr+" "+"BPM, "+t+"m ago"));
 
 
         //"I'm stressed" button
