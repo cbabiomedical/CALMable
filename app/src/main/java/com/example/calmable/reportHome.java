0e178245e9bcc90674dee8bafe62f047f4bad102
AyeshikaWickramasinghe <35-se-0035@kdu.ac.lk>
2021-10-25 17:48:21 +0530

    Report page - daily

--- app/src/main/java/com/example/calmable/reportHome.java
+++ app/src/main/java/com/example/calmable/reportHome.java
@@ -4,11 +4,390 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+//import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+import com.github.mikephil.charting.data.BubbleEntry;
+import com.github.mikephil.charting.data.CandleEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
 public class reportHome extends AppCompatActivity {
+
+    BarChart barChartdaily;
+    private Context context;
+    AppCompatButton monthly, yearly, weekly, whereAmI;
+    File fileName, localFile;
+    FirebaseUser mUser;
+    String text;
+    ArrayList<String> list = new ArrayList<>();
+    ArrayList<Float> floatList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_home);
+
+        barChartdaily = (BarChart) findViewById(R.id.barChartDaily);
+        monthly = findViewById(R.id.monthly);
+        yearly = findViewById(R.id.yearly);
+        weekly = findViewById(R.id.weekly);
+        // whereAmI = findViewById(R.id.whereAmI);
+
+
+
+        //Initializing arraylist and storing input data to arraylist
+        ArrayList<Float> obj = new ArrayList<>(
+                Arrays.asList(30f, 86f, 10f, 50f, 20f, 60f, 80f));
+        //Writing data to file
+        try {
+            fileName = new File(getCacheDir() + "/reportDaily.txt");
+            String line = "";
+            FileWriter fw;
+            fw = new FileWriter(fileName);
+            BufferedWriter output = new BufferedWriter(fw);
+            int size = obj.size();
+            for (int i = 0; i < size; i++) {
+                output.write(obj.get(i).toString() + "\n");
+//                Toast.makeText(this, "Success Writing", Toast.LENGTH_SHORT).show();
+            }
+            output.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        mUser.getUid();
+
+        // Uploading file created to firebase storage
+        StorageReference storageReference1 = FirebaseStorage.getInstance().getReference(mUser.getUid());
+        try {
+            StorageReference mountainsRef = storageReference1.child("reportDaily.txt");
+            InputStream stream = new FileInputStream(new File(fileName.getAbsolutePath()));
+            UploadTask uploadTask = mountainsRef.putStream(stream);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                    Toast.makeText(ConcentrationReportDaily.this, "File Uploaded", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+//                    Toast.makeText(ConcentrationReportDaily.this, "File Uploading Failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        final Handler handler = new Handler();
+        final int delay = 5000;
+
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                StorageReference storageReference = FirebaseStorage.getInstance().getReference(mUser.getUid() + "/reportDaily.txt");
+                //downloading the uploaded file and storing in arraylist
+                try {
+                    localFile = File.createTempFile("tempFile", ".txt");
+                    text = localFile.getAbsolutePath();
+                    Log.d("Bitmap", text);
+                    storageReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+//                            Toast.makeText(ConcentrationReportDaily.this, "Success", Toast.LENGTH_SHORT).show();
+
+                            try {
+                                InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(localFile.getAbsolutePath()));
+
+                                Log.d("FileName", localFile.getAbsolutePath());
+
+                                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                                String line = "";
+
+                                Log.d("First", line);
+                                if ((line = bufferedReader.readLine()) != null) {
+                                    list.add(line);
+                                }
+                                while ((line = bufferedReader.readLine()) != null) {
+
+                                    list.add(line);
+                                    Log.d("Line", line);
+                                }
+
+                                Log.d("List", String.valueOf(list));
+
+                                for (int i = 0; i < list.size(); i++) {
+                                    floatList.add(Float.parseFloat(list.get(i)));
+                                    Log.d("FloatArrayList", String.valueOf(floatList));
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            String[] days = new String[]{"Mon", "Thu", "Wed", "Thur", "Fri", "Sat", "Sun"};
+                            List<Float> creditsMain = new ArrayList<>(Arrays.asList(90f, 30f, 70f, 50f, 10f, 15f, 85f));
+                            float[] strengthDay = new float[]{90f, 30f, 70f, 50f, 10f, 15f, 85f};
+
+                            List<BarEntry> entries = new ArrayList<>();
+                            for (int j = 0; j < floatList.size(); ++j) {
+                                entries.add(new BarEntry(j, floatList.get(j)));
+                            }
+                            float textSize = 16f;
+                            //Initializing object of MyBarDataset class
+                            MyBarDataset dataSet = new MyBarDataset(entries, "data", creditsMain);
+                            dataSet.setColors(ContextCompat.getColor(getApplicationContext(), R.color.black),
+                                    ContextCompat.getColor(getApplicationContext(), R.color.teal_100),
+                                    ContextCompat.getColor(getApplicationContext(), R.color.teal_700),
+                                    ContextCompat.getColor(getApplicationContext(), R.color.dark_blue_300),
+                                    ContextCompat.getColor(getApplicationContext(), R.color.purple_500));
+                            BarData data = new BarData(dataSet);
+                            data.setDrawValues(false);
+                            data.setBarWidth(0.9f);
+
+                            barChartdaily.setData(data);
+                            barChartdaily.setFitBars(true);
+                            barChartdaily.getXAxis().setValueFormatter(new IndexAxisValueFormatter(days));
+                            barChartdaily.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+                            barChartdaily.getXAxis().setTextSize(textSize);
+                            barChartdaily.getAxisLeft().setTextSize(textSize);
+                            barChartdaily.setExtraBottomOffset(10f);
+
+                            barChartdaily.getAxisRight().setEnabled(false);
+                            Description desc = new Description();
+                            desc.setText("");
+                            barChartdaily.setDescription(desc);
+                            barChartdaily.getLegend().setEnabled(false);
+                            barChartdaily.getXAxis().setDrawGridLines(false);
+                            barChartdaily.getAxisLeft().setDrawGridLines(false);
+
+                            barChartdaily.invalidate();
+
+//
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+//                            Toast.makeText(ConcentrationReportDaily.this, "Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                }
+            }
+
+            //Downloading file and displaying chart
+        }, delay);
+
+
+        // On click listener of weekly button
+        weekly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ReportWeekly.class);
+                startActivity(intent);
+            }
+        });
+//        // On click listener of monthly button
+//        monthly.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), ConcentrationReportMonthly.class);
+//                startActivity(intent);
+//            }
+//        });
+//        // On click listener of yearly button
+//        yearly.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), ConcentrationReportYearly.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        // On click listener of where am i toggle button
+//        whereAmI.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), ConcentrationReportWhereamI.class);
+//                startActivity(intent);
+//            }
+//        });
     }
+
+
+    public class MyBarDataset extends BarDataSet {
+
+        private List<Float> credits;
+
+        MyBarDataset(List<BarEntry> yVals, String label, List<Float> credits) {
+            super(yVals, label);
+            this.credits = credits;
+        }
+
+        @Override
+        public int getColor(int index) {
+            float c = credits.get(index);
+
+            if (c > 80) {
+                return mColors.get(0);
+            } else if (c > 60) {
+                return mColors.get(1);
+            } else if (c > 40) {
+                return mColors.get(2);
+            } else if (c > 20) {
+                return mColors.get(3);
+            } else {
+                return mColors.get(4);
+            }
+
+        }
+    }
+
+    private class MyXAxisValueFormatter extends ValueFormatter {
+        private String[] mValues;
+
+        public MyXAxisValueFormatter(String[] values) {
+            this.mValues = values;
+            Log.d("Tag", "monthly clicked hi");
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            return mValues[(int) value];
+        }
+
+    }
+
+    public abstract class ValueFormatter implements IAxisValueFormatter, IValueFormatter {
+
+
+        @Override
+        @Deprecated
+        public String getFormattedValue(float value, AxisBase axis) {
+            return getFormattedValue(value);
+        }
+
+
+        @Override
+        @Deprecated
+        public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+            return getFormattedValue(value);
+        }
+
+
+        public String getFormattedValue(float value) {
+            return String.valueOf(value);
+        }
+
+
+        public String getAxisLabel(float value, AxisBase axis) {
+            return getFormattedValue(value);
+        }
+
+
+        public String getBarLabel(BarEntry barEntry) {
+            return getFormattedValue(barEntry.getY());
+        }
+
+
+        public String getBarStackedLabel(float value, BarEntry stackedEntry) {
+            return getFormattedValue(value);
+        }
+
+
+        public String getPointLabel(Entry entry) {
+            return getFormattedValue(entry.getY());
+        }
+
+
+        public String getPieLabel(float value, PieEntry pieEntry) {
+            return getFormattedValue(value);
+        }
+
+
+        public String getRadarLabel(RadarEntry radarEntry) {
+            return getFormattedValue(radarEntry.getY());
+        }
+
+
+        public String getBubbleLabel(BubbleEntry bubbleEntry) {
+            return getFormattedValue(bubbleEntry.getSize());
+        }
+
+
+        public String getCandleLabel(CandleEntry candleEntry) {
+            return getFormattedValue(candleEntry.getHigh());
+        }
+
+    }
+
+//
+//    public void monthly(View v) {
+//        Intent intent2 = new Intent(this, ConcentrationReportMonthly.class);
+//        startActivity(intent2);
+//
+//    }
+//
+//    public void yearly(View view) {
+//        Intent intent2 = new Intent(this, ConcentrationReportYearly.class);
+//        startActivity(intent2);
+//    }
+//
+//    public void weekly(View view) {
+//        Intent intent2 = new Intent(this, ConcentrationReportWeekly.class);
+//        startActivity(intent2);
+//    }
+
 }
