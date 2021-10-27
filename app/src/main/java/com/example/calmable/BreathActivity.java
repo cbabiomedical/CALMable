c4566583163bb8a59ab3233c829ca18a09994b39
Mahidi99 <cbacdabiomedical@gmail.com>
2021-10-27 08:59:34 +0530

    Updated Breathing Exercise by Mahidi

--- app/src/main/java/com/example/calmable/BreathActivity.java
+++ app/src/main/java/com/example/calmable/BreathActivity.java
@@ -2,13 +2,31 @@ package com.example.calmable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BreathActivity extends AppCompatActivity {
+
+    private Button breathLevelBtn1, breathLevelBtn2,breathLevelBtn3, breathLevelBtn4, breathLevelBtn5 ,breathLevelBtn6, breathLevelBtn7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breath);
+
+        //"I'm stressed" button
+        breathLevelBtn1 = findViewById(R.id.breathLevelBtn1);
+        breathLevelBtn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(BreathActivity.this, breathLevel1.class);
+                startActivity(intent);
+                //startActivity(new Intent(getActivity(),breathLevel1.class));
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/calmable/breathHome.java
+++ app/src/main/java/com/example/calmable/breathHome.java
@@ -2,13 +2,31 @@ package com.example.calmable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class breathHome extends AppCompatActivity {
+    private Button level1Btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breath_home);
+
+        //"I'm stressed" button
+        level1Btn = findViewById(R.id.level1Btn);
+        level1Btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(breathHome.this, breathLevel1.class);
+                startActivity(intent);
+                //startActivity(new Intent(getActivity(),breathLevel1.class));
+
+            }
+        });
     }
 }
