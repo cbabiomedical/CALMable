f17b0b34730f554b347dd80175ac26259eee5121
Mahidi99 <cbacdabiomedical@gmail.com>
2021-11-29 11:03:55 +0530

    Updates on quiz page by Mahidi

--- app/src/main/java/com/example/calmable/ChallengeFragment.java
+++ app/src/main/java/com/example/calmable/ChallengeFragment.java
@@ -69,7 +69,7 @@ public class ChallengeFragment extends Fragment {
         binding.leaderboard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getContext(), Challenge.class));
+                startActivity(new Intent(getContext(), BreathHome.class));
             }
         });
 
