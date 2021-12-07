07437c84e90bb80d2640dcb34f05f0a0c9c46af4
Hasindu5512 <w.hlmendis@gmail.com>
2021-12-07 15:23:12 +0530

    update some uis

--- app/src/main/java/com/example/calmable/MusicSuggestionActivity.java
+++ app/src/main/java/com/example/calmable/MusicSuggestionActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -128,5 +129,9 @@ public class MusicSuggestionActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    public void btnGoSugBreathing (View view) {
+        startActivity(new Intent(getApplicationContext() , BreathPatterns.class));
     }
 }
