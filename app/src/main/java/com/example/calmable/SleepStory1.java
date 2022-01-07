7862a2128b6a609088d14d8960599e01cd64af81
Mahidi99 <cbacdabiomedical@gmail.com>
2022-01-07 12:42:15 +0530

    Updated Sleep story UIs & Button modifications by Mahidi

--- app/src/main/java/com/example/calmable/SleepStory1.java
+++ app/src/main/java/com/example/calmable/SleepStory1.java
@@ -3,12 +3,15 @@ package com.example.calmable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SleepStory1 extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep_story1);
+
     }
 }
