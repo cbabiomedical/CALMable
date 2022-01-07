f266b8d38332d705e714e2edaad8aa5f60c2a542
Mahidi99 <cbacdabiomedical@gmail.com>
2022-01-07 16:32:09 +0530

    Updated dark theme of Sleep story by Mahidi

--- app/src/main/java/com/example/calmable/Sleep3DarkTheme.java
+++ app/src/main/java/com/example/calmable/Sleep3DarkTheme.java
@@ -2,7 +2,9 @@ package com.example.calmable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Sleep3DarkTheme extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Sleep3DarkTheme extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep3_dark_theme);
+    }
+
+    //to change the dark theme to light theme
+    public void btnTheme (View view){
+        startActivity(new Intent(getApplicationContext(), SleepStory3.class));
     }
 }
